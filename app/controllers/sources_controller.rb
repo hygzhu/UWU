b636bc1fbd8801d1f5a0d106c0429cf1357a3ac2
haoyang zhu <hygzhu@gmail.com>
2019-04-08 20:45:37 -0700

    Added source filter

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,7 +1,28 @@
 class SourcesController < ApplicationController
 
     def index
-        @sources = Source.all.paginate(page: params[:page], per_page: 15)
+
+        @filterrific = initialize_filterrific(
+            Source,
+            params[:filterrific],
+            select_options: {
+              sorted_by: Source.options_for_sorted_by
+            },
+            persistence_id: false,
+            default_filter_params: { sorted_by: 'year_desc' },
+            available_filters: [
+              :sorted_by,
+              :search_query
+            ],
+            sanitize_params: true,
+          ) || return
+
+        @sources = @filterrific.find.page(params[:page]).per_page(10)
+       
+        respond_to do |format|
+            format.html
+            format.js
+        end
     end
 
     def show
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,57 @@
 class Source < ApplicationRecord
     has_many :songs, foreign_key: "source_id"
+
+    filterrific(
+        default_filter_params: { sorted_by: 'year_desc' },
+        available_filters: [
+          :sorted_by,
+          :search_query
+        ]
+    )
+
+    scope :search_query, lambda { |query|
+    return nil  if query.blank?
+    # condition query, parse into individual keywords
+    terms = query.downcase.split(/\s+/)
+    # replace "*" with "%" for wildcard searches,
+    # append '%', remove duplicate '%'s
+    terms = terms.map { |e|
+        (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+    }
+    # configure number of OR conditions for provision
+    # of interpolation arguments. Adjust this if you
+    # change the number of OR conditions.
+    num_or_conditions = 2
+    where(
+        terms.map {
+        or_clauses = [
+            "LOWER(sources.name) LIKE ?",
+            "LOWER(sources.period) LIKE ?",
+        ].join(' OR ')
+        "(#{ or_clauses })"
+        }.join(' AND '),
+        *terms.map { |e| ["%#{e}%"] * num_or_conditions }.flatten
+    )
+    }
+
+    scope :sorted_by, lambda { |sort_option|
+    # extract the sort direction from the param value.
+    direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+    case sort_option.to_s
+    when /^year_/
+        order("sources.year #{ direction }")
+    else
+        raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
+    end
+    }
+
+    # This method provides select options for the `sorted_by` filter select input.
+    # It is called in the controller as part of `initialize_filterrific`.
+    def self.options_for_sorted_by
+    [
+        ["Year (Oldest first)", "year_asc"],
+        ["Year (Newest first)", "year_desc"],
+    ]
+    end
+
 end
